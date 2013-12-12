0c85e8d5ee7f6ce9f908afa86c229b61949cf6d5
Wang Wei <voidstarting@gmail.com>
2013-12-12 13:23:26 +0800

    add total price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,13 @@
 class Cart < ActiveRecord::Base
 	has_many :cart_items
 
+	def total_price
+		sum = 0
+		cart_items.each do |c|
+			sum += c.product.price * c.quantity
+		end
+		sum
+	end
 
 	def add_product(product_id)
 		existing_cart_item = cart_items.find_by_product_id(product_id)
