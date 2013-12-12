6c04a7c3c772669d06908a4b6c6166bf38cc01d4
Wang Wei <voidstarting@gmail.com>
2013-12-12 13:19:13 +0800

    better cart view

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,7 +5,7 @@ class Cart < ActiveRecord::Base
 	def add_product(product_id)
 		existing_cart_item = cart_items.find_by_product_id(product_id)
 		if existing_cart_item
-			existing_cart_item.quantity = existing_cart_item.quantity + 1
+			existing_cart_item.quantity += 1
 		else
 			existing_cart_item = cart_items.build(product_id: product_id)
 		end
