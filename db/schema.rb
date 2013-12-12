21762d41bcd1e1438dac2cc11e9ca757241733c4
Wang Wei <voidstarting@gmail.com>
2013-12-12 13:16:10 +0800

    Really change default

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131212051045) do
+ActiveRecord::Schema.define(version: 20131212051510) do
 
   create_table "cart_items", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "cart_id"
     t.integer  "product_id"
-    t.integer  "quantity"
+    t.integer  "quantity",   default: 1
   end
 
   create_table "carts", force: true do |t|
