2347d472bbcd3dc4c2e69b6cd491b186bfcaeb27
Wang Wei <voidstarting@gmail.com>
2013-12-13 09:40:25 +0800

    add password as strong, supposed to be some kind of hash

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213013828) do
+ActiveRecord::Schema.define(version: 20131213013924) do
 
   create_table "cart_items", force: true do |t|
     t.datetime "created_at"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20131213013828) do
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password"
   end
 
 end
