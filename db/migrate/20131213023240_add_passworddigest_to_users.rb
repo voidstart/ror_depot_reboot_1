e3cb0b897efbd11fe9d8b8874b58d9e757167f64
Wang Wei <voidstarting@gmail.com>
2013-12-13 10:44:51 +0800

    add password model migration, form

---
+++ db/migrate/20131213023240_add_passworddigest_to_users.rb
@@ -0,0 +1,5 @@
+class AddPassworddigestToUsers < ActiveRecord::Migration
+  def change
+  	rename_column :users, :password, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213013924) do
+ActiveRecord::Schema.define(version: 20131213023240) do
 
   create_table "cart_items", force: true do |t|
     t.datetime "created_at"
@@ -39,7 +39,8 @@ ActiveRecord::Schema.define(version: 20131213013924) do
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password"
+    t.string   "password_digest"
+    t.string   "username"
   end
 
 end
