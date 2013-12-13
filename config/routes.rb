ccfa9377faa61e43baea782a7664b2877c17a095
Wang Wei <voidstarting@gmail.com>
2013-12-13 09:38:50 +0800

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 App::Application.routes.draw do
+  resources :users
+
   get "store/index"
 
   resources :carts
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131212051510) do
+ActiveRecord::Schema.define(version: 20131213013828) do
 
   create_table "cart_items", force: true do |t|
     t.datetime "created_at"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 20131212051510) do
     t.string   "title"
     t.string   "author"
     t.string   "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
