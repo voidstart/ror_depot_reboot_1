60d1ba6d8e77ac99cdc63ef98d64f84926d766e8
Wang Wei <voidstarting@gmail.com>
2013-12-16 22:27:00 +0800

    upon session create, keep state of logged in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	un = params[:name]
+  	user = User.find_by_username(un)
+  	pswd = params[:password]
+  	if user and user.authenticate(pswd)?
+  		sessions[:user_id] = user.id
+  		redirect_to admin_url
+  	else
+  		redirect_to admin_url, notice "Invalid Username/Password combination"
+  	end
   end
 
   def destory
