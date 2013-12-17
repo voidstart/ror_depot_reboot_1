7620dace426b3a3717b99aab6efd3e1337dda3a1
Wang Wei <voidstarting@gmail.com>
2013-12-17 11:19:19 +0800

    Login ok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	un = params[:name]
+  	un = params[:username]
   	user = User.find_by_username(un)
   	pswd = params[:password]
-  	if user and user.authenticate(pswd)?
-  		sessions[:user_id] = user.id
-  		redirect_to admin_url
+  	if user and user.authenticate(pswd)
+  		session[:user_id] = user.id
+  		redirect_to admin_url, alert: "Login Successful"
   	else
-  		redirect_to admin_url, notice "Invalid Username/Password combination"
+  		redirect_to admin_url, alert: "Invalid Username/Password combination: #{un}"
   	end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 App::Application.routes.draw do
-  get "admin/index"
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destory"
+  get "admin" => "admin#index"
+
+  controller :sessions do
+    get "login" => :new
+    post "login" => :create
+    delete "logout" => :destory
+  end
+
   resources :users
 
   get "store/index"
