a59091876f599b20ca207e0be1f963287f8750a3
Wang Wei <voidstarting@gmail.com>
2013-12-22 21:00:12 +0800

    layout gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.ui.all
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
